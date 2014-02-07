1617b25d57add1af46d5571f014aa5ecc8032c71
Gigi <gigiho609@gmail.com>
2014-02-07 21:25:36 +0100

    Create all required Activity
    - modified so that the desired view layout can be loaded

---
+++ src/se/kth/csc/iprog/dinnerplanner/android/view/WelcomeView.java
@@ -0,0 +1,23 @@
+package se.kth.csc.iprog.dinnerplanner.android.view;
+
+import se.kth.csc.iprog.dinnerplanner.android.R;
+import se.kth.csc.iprog.dinnerplanner.model.DinnerModel;
+import android.view.View;
+import android.widget.TextView;
+
+public class WelcomeView {
+
+	View view;
+
+	public WelcomeView(View view) {
+
+		// store in the class the reference to the Android View
+		this.view = view;
+
+//		TextView example = (TextView) view.findViewById(R.id.example_text);
+//		example.setText("[gg]Welcome to Dinner Planner!Here you will be able to easily plan your dinner.On the next page start by selecting number of participants.Then select your courses.Watch Magic Happen!");
+
+		// Setup the rest of the view layout
+	}
+
+}
