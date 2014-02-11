ba75d82f3bddbb960b9dae97e0c5ed7dafbbc937
svanza <svanai@gmail.com>
2014-02-11 07:45:12 +0100

    Adding intent to android manifest + trying to get summary activity working

--- src/se/kth/csc/iprog/dinnerplanner/android/SummaryActivity.java
+++ src/se/kth/csc/iprog/dinnerplanner/android/SummaryActivity.java
@@ -1,8 +1,11 @@
 package se.kth.csc.iprog.dinnerplanner.android;
 
+import se.kth.csc.iprog.dinnerplanner.android.view.SummaryView;
+import se.kth.csc.iprog.dinnerplanner.model.DinnerModel;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import se.kth.csc.iprog.dinnerplanner.android.R;
 
 public class SummaryActivity extends Activity {
 
@@ -10,6 +13,11 @@ public class SummaryActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_summary);
+
+		DinnerModel model = ((DinnerPlannerApplication) this.getApplication()).getModel();
+		
+		SummaryView mainView = new SummaryView(this, findViewById(R.id.activity_summary_id), model);
+		
 	}
 
 	@Override
